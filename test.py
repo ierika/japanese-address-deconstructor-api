cd298ed25b4c94f9d70e5c8033d0f944af07fa45
Erika <haloerika@gmail.com>
2018-11-18 04:45:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from parser.address_parser import AddressParser
 
 if __name__ == "__main__":
     deconstructed = AddressParser(
-            "2270026神奈川県横浜市青葉区青葉台1-9-2-304"
+            "2270026神奈川県横浜市青葉区青葉台1-9"
     )
     print(deconstructed.input_address)
     print(deconstructed.get_output_components())
